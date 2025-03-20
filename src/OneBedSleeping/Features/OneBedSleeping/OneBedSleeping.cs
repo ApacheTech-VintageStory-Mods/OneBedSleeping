d88f9c097b61bcc17f7b6af7d6679cec77cd332b
ApacheTech Solutions <p_matthews@live.co.uk>
2025-03-20 23:38:59 +0000

    fix: loggers stop mods working in 1.20.6

--- src/OneBedSleeping/Features/OneBedSleeping/OneBedSleeping.cs
+++ src/OneBedSleeping/Features/OneBedSleeping/OneBedSleeping.cs
@@ -10,7 +10,7 @@ public sealed class OneBedSleeping : UniversalModSystem, IServerServiceRegistrar
 
     public void ConfigureServerModServices(IServiceCollection services, ICoreServerAPI sapi)
     {
-        ApiEx.Logger.VerboseDebug("Adding OneBedSleeping feature settings to IOC.");
+        G.Log("Adding OneBedSleeping feature settings to IOC.");
         services.AddFeatureWorldSettings<OneBedSleepingSettings>();
     }
 
@@ -21,19 +21,19 @@ public sealed class OneBedSleeping : UniversalModSystem, IServerServiceRegistrar
 
         var command = sapi.ChatCommands.Create("obs")
             .RequiresPrivilege(Privilege.controlserver)
-            .WithDescription(LangEntry("Command.Description"))
+            .WithDescription(T("Command.Description"))
             .HandleWith(DisplaySettings);
 
         command
             .BeginSubCommand("hunger")
-            .WithDescription(LangEntry("Command.Hunger.Description"))
+            .WithDescription(T("Command.Hunger.Description"))
             .WithArgs(parsers.OptionalFloat("multiplier"))
             .HandleWith(OnHungerSubCommand)
             .EndSubCommand();
 
         command
             .BeginSubCommand("players")
-            .WithDescription(LangEntry("Command.Players.Description"))
+            .WithDescription(T("Command.Players.Description"))
             .WithArgs(parsers.OptionalFloat("percentage"))
             .HandleWith(OnPercentSubCommand)
             .EndSubCommand();
@@ -42,8 +42,8 @@ public sealed class OneBedSleeping : UniversalModSystem, IServerServiceRegistrar
     private TextCommandResult DisplaySettings(TextCommandCallingArgs args)
     {
         var sb = new StringBuilder();
-        sb.AppendLine(LangEntry("Command.Players", _settings.PlayerThreshold * 100));
-        sb.AppendLine(LangEntry("Command.Hunger", _settings.SaturationMultiplier * 100));
+        sb.AppendLine(T("Command.Players", _settings.PlayerThreshold * 100));
+        sb.AppendLine(T("Command.Hunger", _settings.SaturationMultiplier * 100));
         return TextCommandResult.Success(sb.ToString());
     }
 
@@ -51,16 +51,16 @@ public sealed class OneBedSleeping : UniversalModSystem, IServerServiceRegistrar
     {
         var value = args.Parsers[0].GetValue().To<float?>().GetValueOrDefault(1f);
         _settings.SaturationMultiplier = GameMath.Clamp(value, 0f, 2f);
-        return TextCommandResult.Success(LangEntry("Command.Hunger", _settings.SaturationMultiplier * 100));
+        return TextCommandResult.Success(T("Command.Hunger", _settings.SaturationMultiplier * 100));
     }
 
     private TextCommandResult OnPercentSubCommand(TextCommandCallingArgs args)
     {
         var value = args.Parsers[0].GetValue().To<float?>().GetValueOrDefault(0f);
         _settings.PlayerThreshold = GameMath.Clamp(value / 100, 0f, 1f);
-        return TextCommandResult.Success(LangEntry("Command.Players", _settings.PlayerThreshold * 100));
+        return TextCommandResult.Success(T("Command.Players", _settings.PlayerThreshold * 100));
     }
 
-    private static string LangEntry(string code, params object[] args)
+    private static string T(string code, params object[] args)
         => LangEx.FeatureString("OneBedSleeping", code, args);
 }
