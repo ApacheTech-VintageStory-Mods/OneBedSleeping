018ea8a78885a59152fcf8d0f5b76d5cb24bd2ef
ApacheTech <p_matthews@live.co.uk>
2026-04-23 18:57:31 +0100

    Update to .NET 10, mod v4.0, and game 1.22.0 support
    Upgraded target framework to .NET 10.0 and mod version to 4.0.0. Updated required game dependency to 1.22.0. Changed mod side to "Universal" and adjusted required client/server attributes.

--- src/OneBedSleeping/Properties/AssemblyAttribution.cs
+++ src/OneBedSleeping/Properties/AssemblyAttribution.cs
@@ -1,12 +1,11 @@
-﻿[assembly: ModDependency("game", "1.21.0")]
+﻿[assembly: ModDependency("game", "1.22.0")]
 
 [assembly:ModInfo(
     "One Bed Sleeping",
     "onebedsleeping",
     Description = "For use on SMP servers. Only one person needs to sleep in order to pass the time.",
-    Side = "Server",
-    Version = "3.0.0",
-    RequiredOnClient = true,
+    Side = "Universal",
+    Version = "4.0.0",
     RequiredOnServer = true,
     NetworkVersion = "1.0.0",
     IconPath = "modicon.png",
